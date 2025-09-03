24130075c38b8fdce85c995144ab6912d0f953aa
Ashish Morwal <ashishmorwal001@gmail.com>
2025-09-04 01:07:16 +0530

    Commit

--- server/vite.ts
+++ server/vite.ts
@@ -68,18 +68,18 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(import.meta.dirname, "public");
+  const distPath = path.resolve(import.meta.dirname, "..", "client", "dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
-      `Could not find the build directory: ${distPath}, make sure to build the client first`,
+      `Could not find the build directory: ${distPath}, make sure to run "npm run build"`,
     );
   }
 
   app.use(express.static(distPath));
 
-
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
+
