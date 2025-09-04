dc8402428810d073bf39f1647428b5db8a1ad0d5
Ashish Morwal <126047374+Ashish-Morwal@users.noreply.github.com>
2025-09-04 11:40:15 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -57,9 +57,10 @@ app.use((req, res, next) => {
   }
 
  const port = parseInt(process.env.PORT || "5000", 10);
-const host = process.env.HOST || "localhost"; 
+
+const host = process.env.HOST || "0.0.0.0";
 
 server.listen(port, host, () => {
-  log(`serving on http://${host}:${port}`);
+  log(`Serving on http://${host}:${port}`);
 });
 })();
